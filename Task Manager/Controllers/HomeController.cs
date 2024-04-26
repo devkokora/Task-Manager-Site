1c6855c57281042691d39f82c707602fa53c24fa
devkokora <dev.kokora@gmail.com>
2024-04-27 06:32:52 +0700

    Add View

---
+++ Task Manager/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_Manager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.test = "Chukkadig";
+            return View(ViewBag);
+        }
+    }
+}
